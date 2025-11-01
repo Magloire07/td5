1e344a4f32fc03bab8755c8f8ca48b91b05ed39f
Magloire07 <magloirekombede796@gmail.com>
2025-11-01 14:23:45 +0100

    Add infrastructure tests workflow

--- scripts/tofu/live/ci-cd-permissions/main.tf
+++ scripts/tofu/live/ci-cd-permissions/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 module "oidc_provider" {
-  source = "github.com/brikis98/devops-book//ch5/tofu/modules/github-aws-oidc"
+  source = "github.com/Magloire07/td5//scripts/tofu/modules/github-aws-oidc"
 
   provider_url = "https://token.actions.githubusercontent.com" 
 
 }
 
 module "iam_roles" {
-  source = "github.com/brikis98/devops-book//ch5/tofu/modules/gh-actions-iam-roles"
+  source = "github.com/Magloire07/td5//scripts/tofu/modules/gh-actions-iam-roles"
 
   name              = "lambda-sample"                           
   oidc_provider_arn = module.oidc_provider.oidc_provider_arn    
@@ -18,7 +18,7 @@ module "iam_roles" {
   enable_iam_role_for_testing = true                            
 
   # TODO: fill in your own repo name here!
-  github_repo      = "brikis98/fundamentals-of-devops-examples" 
+  github_repo      = "Magloire07/td5" 
   lambda_base_name = "lambda-sample"                            
 
   enable_iam_role_for_plan  = true                                
--- scripts/tofu/live/tofu-state/backend.tf
+++ scripts/tofu/live/tofu-state/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     # TODO: fill in your own bucket name here!
     bucket         = "fundamentals-of-devops-tofu-state" 
-    key            = "ch5/tofu/live/tofu-state"          
+    key            = "scripts/tofu/live/tofu-state"          
     region         = "us-east-2"                         
     encrypt        = true                                
     # TODO: fill in your own DynamoDB table name here!
